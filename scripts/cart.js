19b601812ca393072874311ee13f33ff42f35e57
Luisa <luisa.santos042@gmail.com>
2023-09-10 23:10:32 -0300

    add function 'removeItem()'

--- scripts/cart.js
+++ scripts/cart.js
@@ -13,3 +13,17 @@ function addItem(itemIndex) {
   quantidade.innerHTML = quantidadeItem[itemIndex];
   custoTotalPorItem.innerHTML = valorTotal[itemIndex].toFixed(2);
 }
+
+function removeItem(itemIndex) {
+  let quantidade = document.getElementById('quantidade' + itemIndex);
+  let custoTotalPorItem = document.getElementById('total' + itemIndex);
+
+  if (quantidadeItem[itemIndex] > 0) {
+    quantidadeItem[itemIndex] -= 1;
+    valorTotal[itemIndex] =
+      Number.parseFloat(valorProduto[itemIndex]) * quantidadeItem[itemIndex];
+
+    quantidade.innerHTML = quantidadeItem[itemIndex];
+    custoTotalPorItem.innerHTML = valorTotal[itemIndex].toFixed(2);
+  }
+}
