0ce1b24c60e03e6481b7c6d6da1f2bc4711fdeaa
Luisa <luisa.santos042@gmail.com>
2023-09-10 22:51:35 -0300

    add function to cart
    add function 'addItem()', add some ids on index

---
+++ scripts/cart.js
@@ -0,0 +1,15 @@
+let valorTotal = [0, 0];
+let valorProduto = [49.99, 64.9];
+let quantidadeItem = [0, 0];
+
+function addItem(itemIndex) {
+  let quantidade = document.getElementById('quantidade' + itemIndex);
+  let custoTotalPorItem = document.getElementById('total' + itemIndex);
+
+  quantidadeItem[itemIndex] += 1;
+  valorTotal[itemIndex] =
+    Number.parseFloat(valorProduto[itemIndex]) * quantidadeItem[itemIndex];
+
+  quantidade.innerHTML = quantidadeItem[itemIndex];
+  custoTotalPorItem.innerHTML = valorTotal[itemIndex].toFixed(2);
+}
